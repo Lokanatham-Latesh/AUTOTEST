2fa10dccf2a1c3af32921fcdb630f1ddb4156c73
lokanathamlatesh <lokanathamlatesh@gmail.com>
2026-01-23 14:17:27 +0530

    fix: migrations

--- backend/alembic/versions/131416675501_seed_user_data.py
+++ backend/alembic/versions/131416675501_seed_user_data.py
@@ -1,7 +1,7 @@
 """seed user data
 
 Revision ID: 131416675501
-Revises: 933a97f90659
+Revises: dce725f2abdb
 Create Date: 2025-12-16 13:19:01.870405
 
 """
@@ -13,7 +13,7 @@ from datetime import datetime
 
 # revision identifiers, used by Alembic.
 revision: str = '131416675501'
-down_revision: Union[str, Sequence[str], None] = '933a97f90659'
+down_revision: Union[str, Sequence[str], None] = 'dce725f2abdb'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -23,8 +23,8 @@ def upgrade() -> None:
     op.execute(f"""
         INSERT INTO user (id, role_id, username, password, name, email, is_active)
         VALUES
-            (1, '1', 'admin@test.com', '{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}', 'Admin User', 'admin@test.com', 1),
-            (2, '2', 'qa@test.com', '{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}', 'QA User', 'qa@test.com', 1)
+            (1, '1', 'lokanathaml', '$2b$12$twZSEb6znRomknJtsyJpZOBJ11vCLs4FPCfy.H1w2yZHov5RxUw52', 'lokanatham latesh', 'lokanathamlatesh@gmail.com', 1),
+            (2, '2', 'lateshl', '$2b$12$deFLn6cuNMZqxMj2vYJ34umuWCCyrP/9imW42utqwWB9IXQIaBuTS', 'Lokesh kumar', 'lokanathamlatesh2003@gmail.com', 1)
     """)
 
 
